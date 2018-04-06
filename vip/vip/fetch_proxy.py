126402b88a633c1f2decda294bbbe8693f9e0a61
xiaotao <wcgxiaotao@163.com>
2018-04-06 07:42:24 -0500

    Update fetch_proxy.py

--- vip/vip/fetch_proxy.py
+++ vip/vip/fetch_proxy.py
@@ -12,7 +12,7 @@ class FetchProxy(object):
 
     def get_proxy(self):
         print("获取proxy")
-        url = 'http://webapi.http.zhimacangku.com/getip?num=20&type=1&pro=&city=0&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions='
+        url = 'http://webapi.http.zhimacangku.com/?num=20&type=1&pro=&city=0&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions='
         try:
             resp = requests.get(url)
             proxy = resp.text
